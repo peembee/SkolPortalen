cc6f9e37ba9acc2354ad9f109ce6b56a0781089f
peembee <113596202+peembee@users.noreply.github.com>
2023-08-21 14:39:30 +0200

    Update SchoolContext.cs

--- SkolPortalen/Data/SchoolContext.cs
+++ SkolPortalen/Data/SchoolContext.cs
@@ -32,7 +32,7 @@ namespace SkolPortalen.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-JN0UGIT\\MSSQLSERVER01;Initial Catalog=SchoolV2;Integrated Security =true");
+                optionsBuilder.UseSqlServer("");
             }
         }
 
